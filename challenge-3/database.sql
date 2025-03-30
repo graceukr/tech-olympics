f925ad30e04c60c015ead970c53ff967f2cb0a1e
graceukr <graceukr@icloud.com>
2025-03-30 12:39:23 -0400

    challenge3

--- challenge-3/database.sql
+++ challenge-3/database.sql
@@ -4,3 +4,19 @@ create table logins (
     login_date DATE NOT NULL,
     PRIMARY KEY (user_id, login_date)
 );
+WITH logins_count AS (
+    SELECT COUNT(*) as recent_logins
+    FROM logins
+    WHERE login_date >= CURRENT_DATE - INTERVAL '7 day'
+),
+total_count AS (
+    SELECT COUNT(*) as total_logins
+    FROM logins
+)
+SELECT (
+        SELECT recent_logins
+        FROM logins_count
+    ) * 100.0 / (
+        SELECT total_logins
+        FROM total_count
+    ) AS percentage_last_seven_days;
