dd9d6cae7c0043cd7e91e31a7b5d663272855056
graceukr <graceukr@icloud.com>
2025-03-30 13:50:57 -0400

    chalenge 8

--- challenge-9/challeng-9.sql
+++ challenge-9/challeng-9.sql
@@ -1,6 +1,12 @@
-SELECT Track.TrackId, Track.Name, t.Milliseconds
-FROM Track t
-JOIN Playlist p ON t.playlist_id = p.playlist_id
-
-WHERE p.Name = 'Quick clips'
-  AND (t.genre = 'dramatic' OR t.genre = 'suspenseful');
+SELECT Track.TrackId,
+  Track.Name,
+  Track.Milliseconds
+FROM Track
+  JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId
+  JOIN Playlist ON Track.TrackId = Playlist.TrackId
+  JOIN Genre on Track.GenreId = Genre.GenreId
+WHERE Playlist.Name = 'Quick clips'
+  AND (
+    t.genre = 'dramatic'
+    OR t.genre = 'suspenseful'
+  );
